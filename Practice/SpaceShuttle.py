7aee69b7fa4161995cbd60d5ac4da0e6650aa8bb
LiamSabalburo28 <834830@mystma.org>
2026-03-25 15:04:50 -0500

    Space Shuttle Practice

---
+++ Practice/SpaceShuttle.py
@@ -0,0 +1,16 @@
+countdown = input("Time until launch: ")
+oxytank = input("Total Oxygen tanks: ")
+foodpack = input("Total food packs: ")
+waterpack = input("Total water packs: ")
+
+print("Countdown: " + countdown)
+print("Oxygen Tanks : " + oxytank)
+print("Food Packs : " + foodpack)
+print("Water Packs : " + waterpack)
+
+oxytank = input(" Recheck Oxygen Tanks: ")
+
+print("Countdown: " + countdown)
+print("Oxygen Tanks : " + oxytank)
+print("Food Packs : " + foodpack)
+print("Water Packs : " + waterpack)
