8ab71f85aef9a1a008a8c6d49ece0a71427e9d6c
LiamSabalburo28 <834830@mystma.org>
2026-04-07 14:29:34 -0500

    MadLibs assignment

---
+++ Assignments/MadLibs.py
@@ -0,0 +1,17 @@
+# This [season] come to the [location] and watch The [royal title] of the [jewlrey].
+# Follow [name] and [name] as they attempt to destroy the one [same jewlrey as before], by throwing it into mount [noun]
+# This [number] minute adventure, filled with [verb], [verb], and [adjective].
+# Coming soon to a [location] near you!
+
+season = input("Give me a season")
+royal = input("Give me a royal title")
+jewlery = input("Give me a type of jewlery")
+name1 = input("Give me a name")
+name2 = input("Give me a name")
+noun = input("Give me a noun")
+number = input("Give me a number")
+verb1 = input("Give me a verb")
+verb2 = input("Give me a verb")
+adj = input("Give me an adjective")
+
+print("This {season} come to the {location} and watch The {royal} of the {jewlrey}. Follow {name1} and {name2} as they attempt to destroy the one {jewlrey}, by throwing it into mount {noun}. This {number} minute adventure, filled with {verb1}, {verb2}, and {adjective}. Coming soon to a {location} near you!")
